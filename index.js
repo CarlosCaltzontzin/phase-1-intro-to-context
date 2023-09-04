9770d01c0c4cb23448e478afda7034bfecdff537
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-09-04 15:48:58 -0500

    phase-1-intro-to-context

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
-// Your code here
+// write your code here
+function createEmployeeRecord(employeeData) {
+    return {
+      firstName: employeeData[0],
+      familyName: employeeData[1],
+      title: employeeData[2],
+      payPerHour: employeeData[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(employeeRecord, dateTime) {
+    const [date, hour] = dateTime.split(' ');
+  
+    employeeRecord.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date,
+    });
+  
+    return employeeRecord;
+  }
+  
+  function createTimeOutEvent(employeeRecord, dateTime) {
+    const [date, hour] = dateTime.split(' ');
+  
+    employeeRecord.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date,
+    });
+  
+    return employeeRecord;
+  }
+  
+  function hoursWorkedOnDate(employeeRecord, date) {
+    const timeInEvent = employeeRecord.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = employeeRecord.timeOutEvents.find(event => event.date === date);
+  
+    if (!timeInEvent || !timeOutEvent) {
+      return 0;
+    }
+  
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    const payRate = employeeRecord.payPerHour;
+  
+    return hoursWorked * payRate;
+  }
+  
+  function allWagesFor(employeeRecord) {
+    const datesWorked = employeeRecord.timeInEvents.map(event => event.date);
+    const totalWages = datesWorked.reduce((total, date) => {
+      return total + wagesEarnedOnDate(employeeRecord, date);
+    }, 0);
+  
+    return totalWages;
+  }
+  
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPayroll, employeeRecord) => {
+      return totalPayroll + allWagesFor(employeeRecord);
+    }, 0);
+  }
